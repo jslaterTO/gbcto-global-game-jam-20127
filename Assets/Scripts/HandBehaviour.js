1a86f13c21dd6d1d7919f12d38e7d755a950e58a
Charlie Jiao <charliejiao@hotmail.com>
2017-01-21 21:45:34 -0500

    added hand script so hand model follows the mouse

---
+++ Assets/Scripts/HandBehaviour.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+public class HandBehaviour extends MonoBehaviour{
+
+    public var cameraHand : Camera;
+    public var pos : Vector3;
+
+    function Start () {
+
+    }
+
+    function Update () {
+        // make the hand track the mouse position always
+        pos = cameraHand.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 14));
+        transform.position = Vector3(pos.x,pos.y,transform.position.z);
+    }
+}
